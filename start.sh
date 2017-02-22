874110c85d88655c4afa8a38aeeec7c3c3b1aedd
eightfold <eightfold@eightfolds-iMac.local>
2017-02-21 22:18:52 -0600

    Another try

--- start.sh
+++ start.sh
@@ -5,12 +5,13 @@ ruby run_setup_if_necessary.rb
 eval $(parse_yaml ./config/settings.yml "settings_")
 
 # The crono server is necessary to be able to schedule presets to run
-#echo "= INIT 1 of 4: Starting crono server..."
 if [ ! -f /tmp/foo.txt ]; then
   echo "" > config/cronotab.rb
 fi
-#RAILS_ENV=development bundle exec crono restart
-#echo "= Done!"
 
 # Start the server bound to all IPs (so you can stream mp3s from other machines) on port specified in config/settings.yml
-rails s -b 0.0.0.0 -p $settings_port
+if [ "$settings_port" -le "1024" ]; then
+  rvmsudo rails s -b 0.0.0.0 -p $settings_port
+else
+  rails s -b 0.0.0.0 -p $settings_port
+fi
